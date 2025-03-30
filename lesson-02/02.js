b39e586c1472abc9f949726fb4e5ef79211e4511
pashec <pachec1710@gmail.com>
2025-03-30 15:47:42 +0300

    lesson2-task3

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -26,3 +26,16 @@ else {
     isWinningDouble = false;
     console.log(isWinningDouble);
 }
+
+
+// let dice1 = Math.floor(Math.random() * 6) + 1; // Генерация случайного числа от 1 до 6
+// let dice2 = Math.floor(Math.random() * 6) + 1; // Генерация случайного числа от 1 до 6
+// let isWinningDouble = (dice1 === dice2) && (dice1 > 3); // Проверка на выигрышный дубль
+//
+// console.log('Первый бросок: ' + dice1);
+// console.log('Второй бросок: ' + dice2);
+// if (isWinningDouble) {
+//     console.log('Выигрышный дубль!');
+// } else {
+//     console.log('Не выигрышный дубль.');
+// }
--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,6 +4,33 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
+const score = 100 // тестовое значение, можно изменять
 let grade
-// your code
+
+
+if (score >= 70 &&  score <= 79) {
+    grade = "C"
+    console.log(grade)
+    }
+else if (score >=0 && score <=49) {
+    grade = "F"
+    console.log(grade)
+}
+else if (score >=50 && score <= 69) {
+    grade = "D"
+    console.log(grade)
+}
+else if (score >=80 && score <= 89 ) {
+    grade = "B"
+    console.log(grade)
+}
+else
+{
+    grade = "A"
+    console.log(grade)
+}
+
+
+
+
+
