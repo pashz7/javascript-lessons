e6066b8c76aa230b0106c4d7efce278a45f8f736
pashec <pachec1710@gmail.com>
2025-04-14 21:12:49 +0300

    complete-task2-lesson4

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -15,4 +15,46 @@
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findUniqueElements() {}
+function findUniqueElements(array) {
+    let uniqueArray = []
+    for (let i = 0; i < array.length; i++) {
+      let result = includesElement(uniqueArray,array[i])
+        if (!result) {
+            uniqueArray.push(array[i]);
+        }
+
+
+
+       }
+
+       return uniqueArray;
+    }
+    console.log(findUniqueElements([1,4,5,6,6,6,7,8,9,9]));
+
+// function myFunc (array) {
+//     let newArray = [];
+//     let otherArray = [];
+//
+//     for (let i = 0; i < array.length; i++) {
+//         let result = includesElement(newArray, array[i]);
+//         if (!result) {
+//
+//             newArray.push(array[i]);
+//
+//         }
+//         else {
+//             otherArray.push(array[i]);
+//         }
+//     }
+//     return otherArray;
+// }
+// console.log(myFunc([1,2,2,2,4,5,5,6,7]));
+
+
+
+
+
+
+
+
+
