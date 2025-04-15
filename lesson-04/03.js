c1202e9f2edd7fa5885a3f17b76362fb29ae522c
pashec <pachec1710@gmail.com>
2025-04-15 19:08:39 +0300

    complete-taks3-lesson4

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -17,4 +17,22 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findCommonElements() {}
+function findCommonElements(array1,array2) {
+    let newArray = [];
+    for (let i = 0; i < array1.length; i++) {
+        let result = includesElement(array2,array1[i]);
+        if (result) {
+            newArray.push(array1[i]);
+        }
+        }
+
+
+    return newArray;
+    }
+
+
+    console.log(findCommonElements([1,2,3,9,4,2],[2,3,4]));
+
+
+
+
