87c6738e88df85cd0afdb66f7101d50578995af4
pashec <pachec1710@gmail.com>
2025-05-04 17:55:07 +0300

    complete-task1-lesson07

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -6,4 +6,26 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords() {}
+function capitalizeWords(string) {
+    let newString = string.split(` `)
+    let result = []
+
+    console.log(newString)
+
+    for (let i = 0; i < newString.length; i++) {
+        let abc = newString[i].charAt(0).toUpperCase() + newString[i].slice(1);
+      result.push(abc)
+
+
+    }
+    return result.join(' ')
+
+    // return string.split(' ').map(word=>{
+    //     const [a,...rest]= word.split('');
+    //     const newA = a.toUpperCase();
+    //     return [newA, ...rest].join('');
+    // }).join(' ');
+}
+console.log(capitalizeWords(`hello world from javascript`))
+
+
